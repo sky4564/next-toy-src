97178a6a37864bc84ace6bf164baa1cf1a52c8cd
sky4564 <qw486512@gmail.com>
2024-10-19 17:32:34 +0900

    spiner axis 문제 해결

--- src/app/components/fn/SpinerLogo.tsx
+++ src/app/components/fn/SpinerLogo.tsx
@@ -4,11 +4,11 @@ import logo from "@/assets/logo.svg"
 export default function SpinerLogo({ size }: { size: number }) {
   return (
     <>
-      <div className="animate-spin">
+      <div className="animate-spin " style={{ width: `${size}px` }}>
         {/* <Image src={logo} alt="logo" className={`w-[${size}px] h-[${size}px]`} /> */}
         {/* 테일윈드 퍼지 과정에서 문제 발생함 이렇게 쓸려면 따로 전체설정 건들여야 해서 간단한건 인라인으로 처리함 */}
         {/* 나머지 css 는 테일윈드로 처리해도 둘다 사용가능 하니깐 문제 x  */}
-        <Image src={logo} alt="logo" style={{ width: `${size}px` }} className="bg-red-300" />
+        <Image src={logo} alt="logo" />
       </div>
     </>
   )
