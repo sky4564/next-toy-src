b9246685903fce1dc6bc069cd508ce84e70dd835
sky4564 <qw486512@gmail.com>
2024-10-19 16:27:51 +0900

    navBar 에 페이지 라우팅 추가 .. onclick 같은 마우스 이벤트 핸들러에서 함수 할당하는 모양에 주의

---
+++ src/app/components/ui/navigation/NavBar.tsx
@@ -0,0 +1,38 @@
+"use client"
+import Image from 'next/image'
+import logo2 from '@/assets/logo.svg'
+
+import { useRouter } from 'next/navigation'
+
+export default function NavBar() {
+  const navItem = ['home', 'store', 'submit']
+  const router = useRouter()
+
+
+  const changePage = (page: string) => () => {
+    switch (page) {
+      case 'home': router.push('/'); break;
+      case 'store': router.push('/store'); break;
+      case 'submit': router.push('/submit'); break;
+    }
+  }
+
+  // function changePage(page: string) {
+  //   return () => {
+  //     switch (page) {
+  //       case 'home': router.push('/'); break;
+  //       case 'store': router.push('/store'); break;
+  //       case 'submit': router.push('/submit'); break;
+  //     }
+  //   }
+  // }
+
+  return (
+    <nav className="container flex items-center justify-between p-4 text-xl bg-blue-500 ">
+      <Image className="" src={logo2} alt='logoimg' width={100}></Image>
+      <div className="text-[65px] flex">
+        {navItem.map((x, i) => <p onClick={changePage(x)} key={i} className="mr-40 text-white hover:text-8xl">{x}</p>)}
+      </div>
+    </nav>
+  )
+}
