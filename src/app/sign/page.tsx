b7a89023e49175efb529b0d7dd0c9f8f7e74c70e
sky4564 <qw486512@gmail.com>
2024-10-24 23:50:12 +0900

    react hook form  기본형 추가. todo: 타입체크 , css 적용 필요

--- src/app/sign/page.tsx
+++ src/app/sign/page.tsx
@@ -1,12 +1,19 @@
+import SignUpForm from "../components/SignUpForm"
 
 
 
 export default function SignPage() {
   return (
     <>
-      <div>
-        this is sign up
+
+      <div className="flex flex-col">
+        <h2>회원 가입 </h2>
+        <SignUpForm />
+        <div>
+          this is sign up
+        </div>
       </div>
     </>
   )
 }
+
